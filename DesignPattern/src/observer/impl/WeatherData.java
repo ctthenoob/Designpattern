f8b7dfd4c5aff2507c88c62e67dfceafb622cfe6
chengtaob <chengtaob@yonyou.com>
2016-12-31 16:47:39 +0800

    a little work.

---
+++ DesignPattern/src/observer/impl/WeatherData.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package observer.impl;
+
+import observer.Observer;
+import observer.Subject;
+
+/**
+ * @author Administrator
+ *
+ */
+public class WeatherData implements Subject {
+
+	/* (non-Javadoc)
+	 * @see observer.Subject#registerObserver(observer.Observer)
+	 */
+	@Override
+	public void registerObserver(Observer o) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see observer.Subject#removeObserver(observer.Observer)
+	 */
+	@Override
+	public void removeObserver(Observer o) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see observer.Subject#notifyObservers()
+	 */
+	@Override
+	public void notifyObservers() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
